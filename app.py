388f3a878de4b3e7fdcff8ef0470f65040812a1c
AdamPI <shirong.bai@colorado.edu>
2017-09-03 14:40:28 -0600

    edit/delete article

--- app.py
+++ app.py
@@ -243,6 +243,56 @@ def add_article():
     return render_template('add_article.html', form=form)
 
 
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+    """
+    edit article 
+    """
+    cur = mysql.connection.cursor()
+
+    result = cur.execute("SELECT * FROM articles where id = %s", [id])
+
+    article = cur.fetchone()
+
+    form = ArticleForm(request.form)
+    form.title.data = article['title']
+    form.body.data = article['body']
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        cur.execute(
+            "UPDATE articles SET title=%s, body=%s WHERE id = %s", (title, body, id))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('Article Updated', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('edit_article.html', form=form)
+
+
+@app.route('/delete_article/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_article(id):
+    cur = mysql.connection.cursor()
+
+    cur.execute("DELETE FROM articles WHERE ID = %s", [id])
+
+    mysql.connection.commit()
+
+    cur.close()
+
+    flash('Article Deleted', 'success')
+
+    return redirect(url_for('dashboard'))
+
+
 if __name__ == '__main__':
     app.secret_key = 'secret666'
     app.run(debug=True)
